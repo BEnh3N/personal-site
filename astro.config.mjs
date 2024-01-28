404eccff8ff578d878a9abd652e9e05eb6037c62
BEnh3N <bhpohlmann@icloud.com>
2024-01-28 06:39:58 -0800

    formatting + css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://benh3n-blog.netlify.app",
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,11 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
-
 export async function GET(context) {
-    return rss({
-        title: 'BEnh3N Blog',
-        description: 'The greatest blog to ever exist',
-        site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-        customData: `<languages>en-us</languages>`,
-    });
+  return rss({
+    title: "BEnh3N Blog",
+    description: "The greatest blog to ever exist",
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    customData: `<languages>en-us</languages>`,
+  });
 }
