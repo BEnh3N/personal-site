4ca9b8dbd14a598753ef17f11f870f4db4535b73
BEnh3N <bhpohlmann@icloud.com>
2024-01-27 19:17:20 -0800

    swapped to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://benh3n-blog.netlify.app"
+  site: "https://benh3n-blog.netlify.app",
+  integrations: [tailwind()]
 });
