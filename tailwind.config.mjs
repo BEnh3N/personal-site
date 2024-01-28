d0b2398e21aca247385b5bbfe4d0e500b1dbe428
BEnh3N <bhpohlmann@icloud.com>
2024-01-28 09:44:17 -0800

    more css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,11 +11,17 @@ export default {
         'body': ["Inter var", "monospace"],
       },
       colors: {
-        'text': '#e6eeea',
-        'background': '#070a09',
-        'primary': '#afcbbd',
-        'secondary': '#416264',
-        'accent': '#7096a1',
+        'background': 'rgb(0, 0, 0)',
+        'backgorund-backdrop': 'rgba(0, 0, 0, 0.5)',
+        'accent': 'rgb(225, 225, 225)',
+        'accent-subtext': 'rgb(110, 110, 110)',
+        'accent-highlight': 'rgba(225, 225, 225, 4%)',
+        'accent-hover': 'rgb(30, 30, 30)',
+        'accent-button': 'rgb(25, 25, 25)',
+        'accent-button-elevated': 'rgb(42, 42, 42)',
+      },
+      boxShadow: {
+        'border': 'inset 0 0 0 1.6px rgba(225, 225, 225, 4%)'
       },
     },
   },
