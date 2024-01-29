06a9eea3c8325b311a4b78628069be7b375444ea
BEnh3N <bhpohlmann@icloud.com>
2024-01-29 12:19:03 -0800

    more random

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,11 +1,17 @@
 import { useState } from "preact/hooks";
 
 export default function Greeting({ messages }) {
-    const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+    const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
 
-    const [greeting, setGreeting] = useState(messages[0])
+    const [greeting, setGreeting] = useState(randomMessage);
 
     return (
-        <button class="font-display italic text-accent-subtext text-sm" onClick={() => setGreeting(randomMessage())}>{greeting}</button>
+        <button class="font-display italic text-accent-subtext text-sm" onClick={() => {
+            var new_greeting = randomMessage();
+            while (new_greeting === greeting) {
+                new_greeting = randomMessage();
+            }
+            setGreeting(new_greeting);
+        }}>{greeting}</button>
     );
 }
