2202fa37aa4412c3192ccd41b8d455a8394e28cf
BEnh3N <bhpohlmann@icloud.com>
2024-07-18 17:43:12 -0700

    made the header more consistently sized for my sanity's sake

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,24 +7,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'display': ['Source Code Pro Variable', ...defaultTheme.fontFamily.serif],
-        'body': ["Inter var", "monospace"],
-      },
-      colors: {
-        'background': 'rgb(0, 0, 0)',
-        'backgorund-backdrop': 'rgba(0, 0, 0, 0.5)',
-        'accent': 'rgb(225, 225, 225)',
-        'accent-subtext': 'rgb(110, 110, 110)',
-        'accent-highlight': 'rgba(225, 225, 225, 4%)',
-        'accent-hover': 'rgb(30, 30, 30)',
-        'accent-hover-transparent': 'rgba(48, 48, 48, .5)',
-        'accent-button': 'rgb(25, 25, 25)',
-        'accent-button-elevated': 'rgb(42, 42, 42)',
-        'glass': 'rgba(25, 25, 25, 0.85)',
-      },
-      boxShadow: {
-        'border': 'inset 0 0 0 0.1rem rgba(225, 225, 225, 4%)',
-        'header': '0 0 0 0.2rem rgba(25, 25, 25, 0.85) inset, 0 0 20px 0 rgba(48, 48, 48, .5)',
+        'display': ['Source Code Pro Variable', "monospace"],
+        'body': ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
