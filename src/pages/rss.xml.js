fe973a9be3fba52ed3fd3a0d7aa4da8220c46d96
BEnh3N <bhpohlmann@icloud.com>
2025-05-01 23:35:37 -0700

    temporarily removed content from rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-const parser = new MarkdownIt();
+// import sanitizeHtml from "sanitize-html";
+// import MarkdownIt from "markdown-it";
+// const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("posts");
@@ -16,9 +16,9 @@ export async function GET(context) {
       description: post.data.description,
       link: `/blog/${post.id}/`,
 
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-      }),
+      // content: sanitizeHtml(parser.render(post.body), {
+      //   allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      // }),
     })),
     customData: `<languages>en-us</languages>`,
   });
