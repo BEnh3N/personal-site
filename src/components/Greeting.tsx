aafb50a27507822c139004977b2c25f37dbafe51
BEnh3N <bhpohlmann@icloud.com>
2025-12-04 15:55:37 -0800

    slightly simplified header

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,19 +1,25 @@
 import { useState } from "preact/hooks";
 
 export default function Greeting({ messages }: { messages: string[] }) {
-    const randMessage = () => messages[Math.floor(Math.random() * messages.length)];
-    
-    const [greeting, setGreeting] = useState(randMessage());
+  const randMessage = () =>
+    messages[Math.floor(Math.random() * messages.length)];
 
-    const updateGreeting = () => {
-        let new_greeting = randMessage();
-        while (new_greeting == greeting) {
-            new_greeting = randMessage();
-        }
-        setGreeting(new_greeting);
+  const [greeting, setGreeting] = useState(randMessage());
+
+  const updateGreeting = () => {
+    let new_greeting = randMessage();
+    while (new_greeting == greeting) {
+      new_greeting = randMessage();
     }
+    setGreeting(new_greeting);
+  };
 
-    return (
-        <button class="italic text-gray-500" onClick={() => updateGreeting()}>// {greeting}</button>
-    );
+  return (
+    <button
+      class="hidden text-gray-500 italic lg:inline"
+      onClick={() => updateGreeting()}
+    >
+      // {greeting}
+    </button>
+  );
 }
