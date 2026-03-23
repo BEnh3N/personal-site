cd8de186173df6b3cfa11f065f91533ca47aad25
BEnh3N <bhpohlmann@icloud.com>
2026-03-22 23:16:15 -0500

    moved message list to inside of preact file instead of passed as prop

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,6 +1,18 @@
 import { useState } from "preact/hooks";
 
-export default function Greeting({ messages }: { messages: string[] }) {
+const messages: string[] = [
+  "experience the greatest website to ever exist",
+  "web dev is way too hard",
+  "help",
+  '"if you wait until the last minute, it only takes a minute to do"',
+  "according to all known laws of aviation...",
+  "you got games on your phone?",
+  "you can't just say perchance",
+  "ai can't take my job because I don't have one",
+  "how strange it is to be anything at all...",
+];
+
+export default function Greeting() {
   const randMessage = () =>
     messages[Math.floor(Math.random() * messages.length)];
 
