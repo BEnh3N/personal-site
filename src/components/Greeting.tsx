6792ce6fc4fac51a0ce80c6fefa8adf558e4031a
BEnh3N <bhpohlmann@icloud.com>
2026-03-26 01:12:41 -0700

    greeting now instantly appears on page load

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,37 +1,32 @@
 import { useState } from "preact/hooks";
 
-const messages: string[] = [
-  "experience the greatest website to ever exist",
-  "web dev is way too hard",
-  "help",
-  '"if you wait until the last minute, it only takes a minute to do"',
-  "according to all known laws of aviation...",
-  "you got games on your phone?",
-  "you can't just say perchance",
-  "ai can't take my job because I don't have one",
-  "how strange it is to be anything at all...",
-];
+export default function Greeting({
+  messages,
+  initialValue = 0,
+}: {
+  messages: string[];
+  initialValue?: number;
+}) {
+  const [index, setIndex] = useState(initialValue);
 
-export default function Greeting() {
-  const randMessage = () =>
-    messages[Math.floor(Math.random() * messages.length)];
+  const randIndex = () => {
+    return Math.floor(Math.random() * messages.length);
+  };
 
-  const [greeting, setGreeting] = useState(randMessage());
-
-  const updateGreeting = () => {
-    let new_greeting = randMessage();
-    while (new_greeting == greeting) {
-      new_greeting = randMessage();
-    }
-    setGreeting(new_greeting);
+  const updateMessage = () => {
+    let newIndex;
+    do {
+      newIndex = randIndex();
+    } while (newIndex == index);
+    setIndex(newIndex);
   };
 
   return (
     <button
       class="hidden text-gray-500 italic lg:inline"
-      onClick={() => updateGreeting()}
+      onClick={updateMessage}
     >
-      // {greeting}
+      // {messages[index]}
     </button>
   );
 }
