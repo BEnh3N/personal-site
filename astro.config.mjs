7e9fb3db0d8e2727d732a8064457eea0ccca7e4c
BEnh3N <bhpohlmann@icloud.com>
2026-04-06 19:14:26 -0700

    literally what has changed?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
-import solidJs from "@astrojs/solid-js";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://benh3n.com",
   prefetch: {
     defaultStrategy: "viewport",
   },
-  integrations: [mdx(), solidJs()],
+  integrations: [mdx()],
 
   vite: {
     plugins: [tailwindcss()],
